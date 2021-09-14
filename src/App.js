375365b22762bf2a5d9cc1603b9d83171e37444c
Hugh A. Miles II <hughmil3s@gmail.com>
2021-09-14 04:17:09 +0300

    Update styles and layout

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ import prompt from "./prompt.png";
 export default function App() {
   return (
     <div className="App">
-      <img src={prompt} width={750} height={750} />
+      {/* <img src={prompt} width={750} height={750} /> */}
+      <h1>🏀</h1>
+      <h1 className="t">Miami Tech Runs</h1>
+      <div className="p">
+        Bringing together the nation's emerging and established Founders, VCs,
+        and Creatives to build community, hoops, and mindfulness
+      </div>
       <br></br>
       <Button
         variant="primary"

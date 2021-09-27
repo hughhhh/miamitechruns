c789f4cbbc5cb78e2e6382bd9c153464b6d19f43
Hugh A. Miles II <hughmil3s@gmail.com>
2021-09-27 11:08:32 -0400

    Added instructions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,68 @@
 import { Button } from "react-bootstrap";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
-import prompt from "./prompt.png";
 
 export default function App() {
   return (
     <div className="App">
-      {/* <img src={prompt} width={750} height={750} /> */}
-      <h1>🏀</h1>
-      <h1 className="t">Miami Tech Runs</h1>
-      <div className="p">
-        Bringing together the nation's emerging and established Founders, VCs,
-        and Creatives to build community, hoops, and mindfulness
-      </div>
-      <br></br>
-      <Button
-        variant="primary"
-        size="lg"
-        onClick={() => {
-          window.location.href = "https://telegram.me/collablandbot?start=VFBDI1RFTCNDT01NIy0xMDAxMDc0NzE2NTUx";
-        }}
-      >
-        Click here to get early access
-      </Button>{" "}
+      <Router>
+        <Switch>
+          <Route path="/join">
+            <Instructions />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
+
+const Home = () => (
+  <div>
+    <h1>🏀</h1>
+    <h1 className="t">Miami Tech Runs</h1>
+    <div className="p">
+      Bringing together the nation's emerging and established Founders, VCs, and
+      Creatives to build community, hoops, and mindfulness
+    </div>
+    <br></br>
+    <Button
+      variant="primary"
+      size="lg"
+      onClick={() => {
+        window.location.href =
+          "https://telegram.me/collablandbot?start=VFBDI1RFTCNDT01NIy0xMDAxMDc0NzE2NTUx";
+      }}
+    >
+      Click here to get early access
+    </Button>{" "}
+  </div>
+);
+
+const Instructions = () => {
+  return (
+    <div className="intructions">
+      <h1>Instructions to join</h1>
+      <div>
+        {" "}
+        1. click{" "}
+        <a href="https://telegram.me/collablandbot?start=VFBDI1RFTCNDT01NIy0xMDAxMDc0NzE2NTUx">
+          this link
+        </a>{" "}
+        to join collab link
+      </div>
+      <div> 2. click `Start` in collab.land chat</div>
+      <div> 3. connect your wallet</div>
+      <div> 3. after you should access to MiamiTechRuns DAO chat</div>
+
+      <img
+        className="tutorial"
+        src="tutorial3.gif"
+        alt="This is an animated gif image, but it does not move"
+      />
+    </div>
+  );
+};

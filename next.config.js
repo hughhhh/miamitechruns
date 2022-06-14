a81d0301145a2932e0a9b2971a4c6f88ab2ab399
hughhhh <hughmil3s@gmail.com>
2022-06-14 16:04:27 +0200

    redirect

--- next.config.js
+++ next.config.js
@@ -3,23 +3,11 @@ module.exports = {
     return [
       {
         source: '/',
-        destination: 'https://mirror.xyz/hughmiles.eth/PCTWxkJ5goOS0bRVG-UqDTglPxyhBKGDoPKiKCL6JN4',
+        destination: 'https://techruns.xyz',
         permanent: false,
         basePath: false
       },
-      {
-        source: '/one',
-        destination: 'https://mgate.io/go/wOwW7Ljb6iF7/',
-        permanent: false,
-        basePath: false
-      },
-      {
-        source: '/one/register',
-        destination: 'https://mgate.io/go/Su5oELsI9aa6/',
-        permanent: false,
-        basePath: false
-      },
-    ]
+     ]
   },
   reactStrictMode: true,
 };
